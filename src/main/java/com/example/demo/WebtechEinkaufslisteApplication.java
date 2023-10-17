ef4227e66b4a63a0a6457b51a4f79389214ff25f
NelliKuhl <nellikuhl@gmx.de>
2023-10-17 14:51:04 +0200

    Initialcommit

---
+++ src/main/java/com/example/demo/WebtechEinkaufslisteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechEinkaufslisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechEinkaufslisteApplication.class, args);
+	}
+
+}
