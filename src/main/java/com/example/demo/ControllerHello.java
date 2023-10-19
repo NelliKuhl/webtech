166f039a3dff7841ec4b1c4dc051c5b2add2bd2d
NelliKuhl <nellikuhl@gmx.de>
2023-10-19 18:11:36 +0200

    Thymeleaf eingefügt + erstes Template

--- src/main/java/com/example/demo/ControllerHello.java
+++ src/main/java/com/example/demo/ControllerHello.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ControllerHello {
 
     @GetMapping("/")
-    public String index() {
-        return "Hello World";
+    public String index(Model model) {
+        model.addAttribute("HelloWorld","Hello World");
+        return "hello";
     }
-
 }
