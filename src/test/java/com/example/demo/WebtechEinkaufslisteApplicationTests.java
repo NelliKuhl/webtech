8ef483be2f618b7dab5033653b0e3fb3da77df93
FangerowMarie <121901857+FangerowMarie@users.noreply.github.com>
2023-12-21 13:43:44 +0100

    test auskommentiert

--- src/test/java/com/example/demo/WebtechEinkaufslisteApplicationTests.java
+++ src/test/java/com/example/demo/WebtechEinkaufslisteApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebtechEinkaufslisteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
